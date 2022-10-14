a13b41af13af38f71c3290e4b49cf15c0fa9eb1a
afonne-cid <delightinbusiness@gmail.com>
2022-10-14 23:36:38 +0100

    update: mycalendar1 with hash_map

--- Medium/#729 My Calendar I.py
+++ Medium/#729 My Calendar I.py
@@ -3,9 +3,22 @@ class MyCalendar:
     def __init__(self):
         self.bookings = []
 
-    def book(self, start: int, end: int) -> bool:       
-        for booking in self.bookings:
-            if (not start >= booking[1]) and (not end <= booking[0]):
+    def book(self, start: int, end: int) -> bool:
+        self.bookings[start] = 1 + self.bookings.get(start, 0)
+        self.bookings[end] = -1 + self.bookings.get(end, 0)
+
+        cnt = 0
+        for i in sorted(self.bookings.keys()):
+            cnt += self.bookings[i]
+            if cnt > 1:
+                self.bookings[start] -= 1
+                self.bookings[end] += 1
                 return False
-        self.bookings.append([start, end])
         return True
+        
+    # def book(self, start: int, end: int) -> bool:       
+    #     for booking in self.bookings:
+    #         if (not start >= booking[1]) and (not end <= booking[0]):
+    #             return False
+    #     self.bookings.append([start, end])
+    #     return True
